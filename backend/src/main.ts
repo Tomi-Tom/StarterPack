21907ef9c807ae8ce3106a36fd4b99ad80fef0ac
Croos3r <dorian.moy@epitech.eu>
2023-11-09 17:39:22 +0100

    chore(backend-dto-validation): added zod dto validation
    Took 28 minutes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as packageJson from "../package.json";
+import { patchNestJsSwagger as zodPatchNestJsSwagger, ZodValidationPipe } from "nestjs-zod";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	// DTO Validation w/ zod
+
+	app.useGlobalPipes(new ZodValidationPipe());
+
+	// Swagger documentation
+
+	zodPatchNestJsSwagger();
 	const swaggerConfiguration = new DocumentBuilder()
 		.setTitle("StarterPack API")
 		.setDescription(packageJson.description)
@@ -12,6 +21,7 @@ async function bootstrap() {
 		.build();
 	const document = SwaggerModule.createDocument(app, swaggerConfiguration);
 	SwaggerModule.setup("documentation", app, document);
+
 	await app.listen(3000);
 }
 
