88ae6c2bbb643d88868028a1021d67e48eb94c89
Croos3r <dorian.moy@epitech.eu>
2023-11-09 16:44:15 +0100

    chore(backend-database-orm): set up typeorm and migration scripts
    Took 1 hour 3 minutes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DATA_SOURCE_CONFIGURATION } from "./utils/database";
 
 @Module({
 	imports: [ ConfigModule.forRoot({
 		isGlobal: true,
 		expandVariables: true
-	}) ],
+	}), TypeOrmModule.forRoot(DATA_SOURCE_CONFIGURATION) ],
 	controllers: [],
 	providers: []
 })
