3274a79ac1b43ec672f5a6511f719919f7fa9fee
Croos3r <dorian.moy@epitech.eu>
2023-11-09 14:06:22 +0100

    docs(backend): set up swagger documentation
    Took 12 minutes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as packageJson from "../package.json";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const swaggerConfiguration = new DocumentBuilder()
+		.setTitle("StarterPack API")
+		.setDescription(packageJson.description)
+		.setVersion(packageJson.version)
+		.build();
+	const document = SwaggerModule.createDocument(app, swaggerConfiguration);
+	SwaggerModule.setup("documentation", app, document);
 	await app.listen(3000);
 }
+
 bootstrap();
