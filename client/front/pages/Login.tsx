c87d4f0cceb0d3d8a62643f8ce477008be57f815
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-11-19 19:28:05 +0100

    feat(client/front): base for login/register pages

--- client/front/pages/Login.tsx
+++ client/front/pages/Login.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-interface LandingProps {
-    sidebarOpen: boolean;
-}
+const Login = () => {
+    const [register, setRegister] = React.useState(false);
 
-const Login: React.FC<LandingProps> = ({ sidebarOpen }) => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px', marginLeft: sidebarOpen ? '250px' : '80px', transition: '0.5s' }}>
+        register ?
+            <div style={{ textAlign: 'center', marginTop: '50px', transition: '0.5s' }}>
+                <h1>Register</h1>
+                <button onClick={() => setRegister(false)}>Login</button>
+            </div> :
+        <div style={{ textAlign: 'center', marginTop: '50px', transition: '0.5s' }}>
             <h1>Login</h1>
-            {sidebarOpen ? <p>Open</p> : <p>Closed</p>}
+            <button onClick={() => setRegister(true)}>Register</button>
         </div>
     );
 };
