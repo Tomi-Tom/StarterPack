9bb26d41705ed621d0d05975cb3d66ced9b5d29f
Croos3r <dorian.moy@epitech.eu>
2024-01-05 07:50:17 +0100

    feat(identifier): added darwin type os
    Took 9 minutes
    
    
    Took 4 seconds

--- client/back/identifier/index.ts
+++ client/back/identifier/index.ts
@@ -1,11 +1,12 @@
-import Handler from "../utils/meta/Handler";
-import * as os from "os";
+import Handler from '../utils/meta/Handler';
+import * as os from 'os';
 
 export default class Identifier {
-	@Handler()
-	static async identify(): Promise<"windows" | "linux"> {
-		if (os.type() == "Windows_NT") return "windows";
-		else if (os.type() == "Linux") return "linux";
-		else throw new Error("Unknown OS");
-	}
+    @Handler()
+    static async identify(): Promise<'windows' | 'linux' | 'darwin'> {
+        if (os.type() == 'Windows_NT') return 'windows';
+        else if (os.type() == 'Linux') return 'linux';
+        else if (os.type() == 'Darwin') return 'darwin';
+        else throw new Error('Unknown OS');
+    }
 }
